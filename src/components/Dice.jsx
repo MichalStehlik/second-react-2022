7ddf237fc96b0a2a1d66b2eb10822bc39305daad
Michal Stehlík <michal.stehlik@pslib.cz>
2022-05-04 11:53:01 +0200

    init

---
+++ src/components/Dice.jsx
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from "react"
+import {Button} from "reactstrap"
+
+/*export const Dice = ({size}) => (
+    <p>{size}</p>
+)*/
+
+export const Dice = ({size, rollHandler}) => {
+    /*const RollAction = useCallback((s) => {
+        setValue(Math.floor(Math.random() * s + 1));
+    },[size]);*/
+    const RollAction = (s) => {
+        setValue(Math.floor(Math.random() * s + 1));      
+    }
+    const [value, setValue] = useState();
+    useEffect(()=>{
+        console.log("Component added");
+    },[]);
+    useEffect(()=>{
+        console.log("Size changed");
+        RollAction(size);
+    },[size]);
+    useEffect(()=>{
+        console.log("Value added");
+        if (rollHandler !== undefined) rollHandler(value);
+    },[value, rollHandler])
+    return (
+        <div>
+        <span className="m-3">{value}</span>
+        <Button onClick={()=>{RollAction(size)}}>Roll</Button>
+        </div>
+        
+    ) 
+        
+}
+    
+
+export default Dice;
